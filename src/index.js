37c550bf5b88e27c2d5257fe0bc333079e5c4d33
rachfuerst <rach.fuerst@gmail.com>
2019-12-14 14:53:13 -0600

    Loading the dog information and photos, need to work on the button

--- src/index.js
+++ src/index.js
@@ -1,26 +1,59 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("This works")
     const dogBar = document.getElementById('dog-bar');
+    const dogInfo= document.getElementById('dog-info');
     const fetchDogs = () => {
         fetch('http://localhost:3000/pups')
             .then(response => response.json())
             .then(data => {
                 let dogsData = data
                 dogBarData(dogsData);
+                return dogsData
         })
 
     }
 
     let dogBarData = (dogsData) => {
+        let span;
        for(element in dogsData) {
            console.log(dogsData[element].name)
-           let span = document.createElement('span');
+           let dogDiv = document.createElement('div');
+           let dogPic = document.createElement('img');
+           let dogName = document.createElement('h2');
+           let dogButton = document.createElement('button');
+           dogButton.name = "Good Dog!";
+           dogName.innerText = `${dogsData[element].name}`
+           dogPic.src = `${dogsData[element].image}`;
+           console.log(dogPic);
+           span = document.createElement('span');
            span.innerHTML= `${dogsData[element].name}`;
+           dogDiv.appendChild(dogPic);
+           dogDiv.appendChild(dogName);
+           dogDiv.appendChild(dogButton);
            dogBar.appendChild(span);
+           span.addEventListener("click", () =>  {
+            dogInfo.appendChild(dogDiv);
+            
+           });
+       }
+        // console.log(span);
+    
            
        }
-    }
+    
 
+    // const dogDetails = () => {
+    //     console.log(dogsData[element])
+    //     let dogDiv = document.createElement('div');
+    //     let dogPic = document.createElement('img');
+    //     dogPic.src = `${dogsData[element].image}`;
+    //     dogDiv.appendChild(dogPic);
+    //     dogInfo.appendChild(dogDiv);
+    //     console.log(dogPic);
+        
+    //    }
+
+  
     fetchDogs();
     
 });
